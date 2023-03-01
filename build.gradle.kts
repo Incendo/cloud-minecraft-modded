2197bdd124c07cffa61298436a35d793bd877e4c
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-03-01 16:13:53 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import net.fabricmc.loom.task.AbstractRunTask
+
+plugins {
+    `java-library`
+    id("dev.architectury.loom") version "1.1-SNAPSHOT"
+    val indraVer = "3.0.1"
+    id("net.kyori.indra") version indraVer
+    id("net.kyori.indra.publishing") version indraVer
+    id("net.kyori.indra.license-header") version indraVer
+}
+
+indra {
+    javaVersions().target(17)
+}
+
+license {
+    header(file("HEADER"))
+}
+
+val transitiveInclude: Configuration by configurations.creating
+
+dependencies {
+    minecraft("com.mojang:minecraft:1.19.3")
+    mappings(loom.officialMojangMappings())
+    forge("net.minecraftforge", "forge", "1.19.3-44.1.0")
+    api(transitiveInclude(forgeExtra(platform("cloud.commandframework:cloud-bom:1.8.2"))!!)!!)
+    api(transitiveInclude(forgeExtra("cloud.commandframework:cloud-core")!!)!!)
+    api(transitiveInclude(forgeExtra("cloud.commandframework:cloud-brigadier")!!)!!)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+configurations.include {
+    withDependencies {
+        transitiveInclude.incoming.resolutionResult.allDependencies.map {
+            (it as ResolvedDependencyResult).selected.id.displayName
+        }.forEach { add(project.dependencies.create(it)) }
+    }
+}
+
+tasks {
+    withType<AbstractRunTask>().configureEach {
+        systemProperty("cloud_forge.test_commands", true)
+    }
+    processResources {
+        inputs.property("version", project.version)
+        filesMatching("META-INF/mods.toml") {
+            expand("version" to project.version)
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.minecraftforge.net")
+        maven("https://maven.architectury.dev/")
+    }
+}
+
+rootProject.name = "cloud-forge"
