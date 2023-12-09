c13d08ccd9b9481c485acb6472c11b28c304bd56
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-12-08 22:05:18 -0700

    Build updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,36 @@
-import net.fabricmc.loom.task.AbstractRunTask
-
 plugins {
     val indraVer = "3.1.3"
     id("net.kyori.indra") version indraVer
     id("net.kyori.indra.publishing") version indraVer
     id("net.kyori.indra.publishing.sonatype") version indraVer
-    id("net.kyori.indra.license-header") version indraVer
+    id("net.kyori.indra.licenser.spotless") version indraVer
     id("xyz.jpenilla.quiet-architectury-loom") version "1.4-SNAPSHOT"
+}
+
+indra {
+    javaVersions {
+        target(17)
+    }
+    github("Incendo", "cloud-neoforge") {
+        ci(true)
+    }
+    mitLicense()
+}
+
+indraSpotlessLicenser {
+    licenseHeaderFile(file("HEADER"))
+}
+
+configurations {
+    transitiveInclude {
+        extendsFrom(api.get())
+
+        exclude("org.checkerframework")
+        exclude("org.apiguardian")
+    }
+    forgeExtra {
+        extendsFrom(api.get())
+    }
 }
 
 repositories {
@@ -15,52 +39,28 @@ repositories {
     maven("https://maven.neoforged.net/releases/")
 }
 
-indra {
-    javaVersions().target(17)
-}
-
-license {
-    header(file("HEADER"))
-}
-
-val transitiveInclude: Configuration by configurations.creating {
-    exclude("org.checkerframework")
-    exclude("org.apiguardian")
-}
-
 dependencies {
     minecraft("com.mojang:minecraft:1.20.2")
     mappings(loom.officialMojangMappings())
-    neoForge("net.neoforged", "neoforge", "20.2.86")
+    neoForge("net.neoforged:neoforge:20.2.86")
 
-    listOf("api", transitiveInclude.name, "forgeExtra").forEach { c ->
-        c(platform("cloud.commandframework:cloud-bom:1.8.4"))
-        c("cloud.commandframework:cloud-core")
-        c("cloud.commandframework:cloud-brigadier")
-    }
-}
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
-
-configurations.include {
-    withDependencies {
-        transitiveInclude.incoming.resolutionResult.allDependencies.map {
-            (it as ResolvedDependencyResult).selected.id.displayName
-        }.forEach { add(project.dependencies.create(it)) }
-    }
+    api(platform("cloud.commandframework:cloud-bom:1.8.4"))
+    api("cloud.commandframework:cloud-core")
+    api("cloud.commandframework:cloud-brigadier")
 }
 
 tasks {
-    withType<AbstractRunTask>().configureEach {
-        systemProperty("cloud.test_commands", true)
-    }
     processResources {
-        inputs.property("version", project.version)
+        val props = mapOf("version" to project.version)
+        inputs.properties(props)
         filesMatching("META-INF/mods.toml") {
-            expand("version" to project.version)
+            expand(props)
         }
+    }
+}
+
+loom {
+    runs.configureEach {
+        vmArg("-Dcloud.test_commands=true")
     }
 }
