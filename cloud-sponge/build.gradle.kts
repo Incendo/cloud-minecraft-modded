bfc567c03d93a4621d87268f7ce5485e60861a3f
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-01-21 12:38:26 -0700

    More updating for 1.20.2

--- cloud-sponge/build.gradle.kts
+++ cloud-sponge/build.gradle.kts
@@ -5,11 +5,10 @@ plugins {
     id("org.spongepowered.gradle.vanilla")
 }
 
-java.disableAutoTargetJvm()
-
 dependencies {
     api(libs.cloud.core)
     implementation(libs.cloud.brigadier)
+    implementation(project(":cloud-minecraft-modded-common", configuration = "namedElements"))
     compileOnly("org.spongepowered:spongeapi:11.0.0-SNAPSHOT")
     compileOnly("org.spongepowered:sponge:1.20.2-11.0.0-SNAPSHOT")
 }
