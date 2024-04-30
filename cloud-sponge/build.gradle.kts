213c8102976477779c047d2a58dc82f13d17edea
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-04-30 14:15:57 -0700

    Fix sponge compile

--- cloud-sponge/build.gradle.kts
+++ cloud-sponge/build.gradle.kts
@@ -11,10 +11,10 @@ dependencies {
     implementation(libs.cloud.brigadier)
     implementation(project(":cloud-minecraft-modded-common", configuration = "namedElements"))
     compileOnly("org.spongepowered:spongeapi:11.0.0-SNAPSHOT")
-    compileOnly("org.spongepowered:sponge:1.20.2-11.0.0-SNAPSHOT")
+    compileOnly("org.spongepowered:sponge:1.20.4-11.0.0-SNAPSHOT")
 }
 
 minecraft {
-    version("1.20.2")
+    version("1.20.4")
     platform(MinecraftPlatform.JOINED)
 }
