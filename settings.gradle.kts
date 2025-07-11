709bd5532cdbf188630bb63e396b4ec65f186339
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-07-11 11:24:16 -0700

    Update cloud-minecraft and build with Minecraft 1.21.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -34,7 +34,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("fabricApiLibs") {
-            from("net.fabricmc.fabric-api:fabric-api-catalog:0.127.1+1.21.6")
+            from("net.fabricmc.fabric-api:fabric-api-catalog:0.129.0+1.21.7")
         }
     }
 }
